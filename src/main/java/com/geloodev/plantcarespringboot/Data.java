7d0b2bad90a6ee9327c33a12168196cb0feab8a7
geloodev <angeloholandini11@gmail.com>
2023-08-17 14:13:24 -0300

    :fire: removing unused classes

--- src/main/java/com/geloodev/plantcarespringboot/Data.java
+++ src/main/java/com/geloodev/plantcarespringboot/Data.java
@@ -4,9 +4,5 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Data {
-    int id;
-    String common_name;
-    String slug;
-    String scientific_name;
-    int year;
+    
 }
