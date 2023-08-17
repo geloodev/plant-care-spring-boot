bf89220abf10098bec9e984dfdaac6dc6d60decc
geloodev <angeloholandini11@gmail.com>
2023-08-17 14:20:43 -0300

    :recycle: using ObjectMapper for deserialization
    + defining baseUrl and token attributes

--- src/main/java/com/geloodev/plantcarespringboot/PlantApiService.java
+++ src/main/java/com/geloodev/plantcarespringboot/PlantApiService.java
@@ -1,6 +1,6 @@
 package com.geloodev.plantcarespringboot;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,30 +8,51 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Service
 public class PlantApiService {
     
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+    private final String baseUrl;
+    private final String token;
 
     @Autowired
     public PlantApiService() {
         this.restTemplate = new RestTemplate();
+        this.objectMapper = new ObjectMapper();
+        this.baseUrl = "https://trefle.io/api/v1/";
+        this.token = "9SrXibl67PyyhQ8tkCvrFaXfAV1_0YvhaVmliyC4HUg";
     }
 
     public List<Plant> getPlantsFromApi() {
-        String url = "https://trefle.io/api/v1/plants?token=9SrXibl67PyyhQ8tkCvrFaXfAV1_0YvhaVmliyC4HUg";
-        ResponseEntity<PlantList> response = restTemplate.getForEntity(url, PlantList.class);
-        PlantList plantList = response.getBody();
-        List<Plant> plants = new ArrayList<Plant>();
-        for (Data data : plantList.getData()) {
-            plants.add(new Plant(data));
+        String url = baseUrl + "plants?token=" + token;
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        try {
+            JsonNode rootNode = objectMapper.readTree(response.getBody());
+            JsonNode dataNode = rootNode.get("data");
+            List<Plant> plants = objectMapper.readValue(dataNode.toString(), new TypeReference<List<Plant>>() {});
+            return plants;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
-        return plants;
     }
 
     public Plant getPlantFromApiById(int id) {
-        String url = "https://trefle.io/api/v1/plants/" + id + "?token=9SrXibl67PyyhQ8tkCvrFaXfAV1_0YvhaVmliyC4HUg";
-        ResponseEntity<Plant> response = restTemplate.getForEntity(url, Plant.class);
-        return response.getBody();
+        String url = baseUrl + "plants/" + id + "?token=" + token;
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);        
+        try {
+            JsonNode rootNode = objectMapper.readTree(response.getBody());
+            JsonNode dataNode = rootNode.get("data");
+            Plant plant = objectMapper.readValue(dataNode.toString(), new TypeReference<Plant>() {});
+            return plant;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
